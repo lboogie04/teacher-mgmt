ef19b35227edde33d305bb40976ae09f78d61180
Lawrence S <lboogie@Lawrences-Air.attlocal.net>
2024-01-23 16:04:43 -0600

    final touches

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,6 +6,7 @@ module Admin
         @total_docs_count = Document.all.count
         @total_teachers_count = User.with_role(:teacher).count
         @total_impressionist_count = Document.all.inject(0) { |sum, document| sum + document.impressionist_count(start_date: Date.today) }
+        @documents_today = Document.includes(:user).where('DATE(created_at) = ?', Date.today)
       end
     end
   end
