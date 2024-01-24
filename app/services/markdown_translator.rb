cb86f480010020b66da08f64be63ea15e3e5bd9c
Lawrence S <lboogie@Lawrences-Air.attlocal.net>
2024-01-23 22:25:56 -0600

    UTF8 encoding

--- app/services/markdown_translator.rb
+++ app/services/markdown_translator.rb
@@ -2,7 +2,13 @@ require 'kramdown'
 
 module MarkdownTranslator
   def self.convert(markdown_text)
+    markdown_text.force_encoding('UTF-8')
+
     html_output = Kramdown::Document.new(markdown_text).to_html
-    return html_output
+
+    # Ensure the output has UTF-8 encoding
+    html_output.force_encoding('UTF-8')
+
+    html_output
   end
 end
